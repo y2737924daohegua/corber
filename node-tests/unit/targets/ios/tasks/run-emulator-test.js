eee9e96c4ac882cc43f013b84fcf53c66bdd7d25
Alex Blom <alex@alexblom.com>
2017-11-17 16:51:24 -0500

    feat(run-emulator): first impl of installing/auto booting iOS emulator

---
+++ node-tests/unit/targets/ios/tasks/run-emulator-test.js
@@ -0,0 +1,47 @@
+const td              = require('testdouble');
+const path            = require('path');
+const Promise         = require('rsvp').Promise;
+const expect          = require('../../../../helpers/expect');
+
+const setupRunTask = function() {
+  let RunTask = require('../../../../../lib/targets/ios/tasks/run-emulator');
+  return new RunTask();
+};
+
+describe('iOS Run Emulator Task', function() {
+  afterEach(function() {
+    td.reset();
+  });
+
+  it ('boots, installs & launches emulator app', function() {
+    let invokes = [];
+    let appPath = path.join(process.cwd(), 'corber', 'tmp/builds/Build/Products/Debug-iphonesimulator/react.app');
+
+
+    td.replace('../../../../../lib/utils/spawn', function(cmd, args) {
+      invokes.push([...arguments]);
+      return Promise.resolve();
+    });
+
+    let runEmulator = setupRunTask();
+    return runEmulator.run('emulator').then(function() {
+      expect(invokes.length).to.equal(3);
+
+      expect(invokes[0]).to.deep.equal([
+        '/usr/bin/xcrun',
+        ['simctl', 'boot', 'emulator']
+      ]);
+
+      expect(invokes[1]).to.deep.equal([
+        '/usr/bin/xcrun',
+        ['simctl', 'install', 'emulator', appPath]
+      ]);
+
+      expect(invokes[2]).to.deep.equal([
+        '/usr/bin/xcrun',
+        ['simctl', 'launch', 'emulator', 'io.corber.react']
+      ]);
+    });
+  });
+});
+
