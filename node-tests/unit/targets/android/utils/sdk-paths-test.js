a33b4338212878ea3647d4ae476a401cd4344119
Jordan Smith <bear.chunda@gmail.com>
2018-04-12 14:30:09 -0500

    feat(android-start) add resolvePath util and sdkLookups to find sdk

--- node-tests/unit/targets/android/utils/sdk-paths-test.js
+++ node-tests/unit/targets/android/utils/sdk-paths-test.js
@@ -1,37 +1,23 @@
-/* global xit */
-const td              = require('testdouble');
-const path            = require('path');
-const expect          = require('../../../../helpers/expect');
+const expect = require('../../../../helpers/expect');
+const path   = require('path');
+const td     = require('testdouble');
 
 describe('Android sdk paths util', function() {
   afterEach(function() {
     td.reset();
   });
 
-  context('darwin', function() {
-    beforeEach(function() {
-      td.replace('../../../../../lib/utils/get-os', function() {
-        return 'darwin';
-      });
+  it('returns a hash with adb & emulator', function() {
+    td.replace('../../../../../lib/targets/android/utils/resolve-path', function() {
+      return 'fake-path';
     });
 
-    it('returns a hash with adb & emulator', function() {
-      let sdkPaths = require('../../../../../lib/targets/android/utils/sdk-paths');
-      let sdkRoot = path.join(process.env['HOME'], 'Library', 'Android', 'sdk');
+    let sdkPaths = require('../../../../../lib/targets/android/utils/sdk-paths');
+    let paths = sdkPaths();
 
-      let paths = sdkPaths();
-
-      expect(paths).to.deep.equal({
-        adb: path.join(sdkRoot, 'platform-tools', 'adb'),
-        emulator: path.join(sdkRoot, 'tools', 'emulator')
-      });
-    });
-  });
-
-  context('win32', function() {
-    xit('returns a hash with adb & emulator', function() {
+    expect(paths).to.deep.equal({
+      adb: path.join('fake-path', 'platform-tools', 'adb'),
+      emulator: path.join('fake-path')
     });
   });
 });
-
-
