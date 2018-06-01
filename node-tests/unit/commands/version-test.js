0f3e04d7ff3a4122ece81f5ad32448aa8a4f0d8f
Alex MacLeod <alexhmacleod@gmail.com>
2018-06-01 15:03:35 -0400

    feat(version-command): report package.json and node_modules corber versions

--- node-tests/unit/commands/version-test.js
+++ node-tests/unit/commands/version-test.js
@@ -1,29 +1,28 @@
 const td          = require('testdouble');
 const mockProject = require('../../fixtures/corber-mock/project');
-
-const logger      = require('../../../lib/utils/logger');
 const contains    = td.matchers.contains;
-
-let versions, infoDouble, versionCmd;
+const root        = mockProject.project.root;
 
 describe('Version Command', () => {
+  let logger, getVersions;
+  let versionCmd;
+
   beforeEach(() => {
-    versions = {
+    logger = td.replace('../../../lib/utils/logger');
+    getVersions = td.replace('../../../lib/utils/get-versions');
+
+    td.when(getVersions(root)).thenReturn({
       corber: {
         global: '1.0.0',
-        project: '2.0.0'
+        project: {
+          required: '>2.0.0',
+          installed: '2.0.1'
+        }
       },
       node: '8.9.4'
-    };
-
-    td.replace('../../../lib/utils/get-versions', (root) => {
-      return versions;
     });
 
-    infoDouble = td.replace(logger, 'info');
-
     let VersionCmd = require('../../../lib/commands/version');
-
     versionCmd = new VersionCmd({
       project: mockProject.project
     });
@@ -36,22 +35,75 @@ describe('Version Command', () => {
   describe('run', () => {
     it('shows corber and node versions', () => {
       return versionCmd.run().then(() => {
-        td.verify(infoDouble('corber (global): 1.0.0'));
-        td.verify(infoDouble('corber (project): 2.0.0'));
-        td.verify(infoDouble('node: 8.9.4'));
+        td.verify(logger.info('corber (global): 1.0.0'));
+        td.verify(logger.info('corber (package.json): >2.0.0'));
+        td.verify(logger.info('corber (node_modules): 2.0.1'));
+        td.verify(logger.info('node: 8.9.4'));
       })
     });
 
-    context('when project version unavailable', () => {
+    context('when global version unavailable', () => {
       beforeEach(() => {
-        delete versions.corber.project;
+        td.when(getVersions(root)).thenReturn({
+          corber: {
+            project: {
+              required: '>2.0.0',
+              installed: '2.0.1'
+            }
+          },
+          node: '8.9.4'
+        });
       });
 
-      it('does not show project version', () => {
+      it('does not show global version', () => {
         return versionCmd.run().then(() => {
-          td.verify(infoDouble(contains('corber (project)')), { times: 0 });
+          td.verify(logger.info(contains('corber (global)')), { times: 0 });
         })
       });
-    })
+    });
+
+    context('when required version unavailable', () => {
+      beforeEach(() => {
+        td.when(getVersions(root)).thenReturn({
+          corber: {
+            global: '1.0.0',
+            project: {
+              installed: '2.0.1'
+            }
+          },
+          node: '8.9.4'
+        });
+      });
+
+      it('does not show required version', () => {
+        return versionCmd.run().then(() => {
+          td.verify(logger.info(contains('corber (package.json)')), {
+            times: 0
+          });
+        })
+      });
+    });
+
+    context('when installed version unavailable', () => {
+      beforeEach(() => {
+        td.when(getVersions(root)).thenReturn({
+          corber: {
+            global: '1.0.0',
+            project: {
+              required: '>2.0.0'
+            }
+          },
+          node: '8.9.4'
+        });
+      });
+
+      it('does not show installed version', () => {
+        return versionCmd.run().then(() => {
+          td.verify(logger.info(contains('corber (node_modules)')), {
+            times: 0
+          });
+        })
+      });
+    });
   });
 });
