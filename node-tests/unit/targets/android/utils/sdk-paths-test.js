c607f04f167fdda3e4c39fa853c00155b81a84c5
Alex Blom <alex@alexblom.com>
2018-10-18 00:33:55 -0400

    feat(start): fixes support for ubuntu/nix, fixes #529

--- node-tests/unit/targets/android/utils/sdk-paths-test.js
+++ node-tests/unit/targets/android/utils/sdk-paths-test.js
@@ -1,23 +1,41 @@
-const expect = require('../../../../helpers/expect');
-const path   = require('path');
-const td     = require('testdouble');
+const expect          = require('../../../../helpers/expect');
+const path            = require('path');
+const td              = require('testdouble');
+const anything        = td.matchers.anything;
 
 describe('Android sdk paths util', function() {
   afterEach(function() {
     td.reset();
   });
 
-  it('returns a hash with adb & emulator', function() {
+  it('returns resolved adb & emulator paths', function() {
     td.replace('../../../../../lib/targets/android/utils/resolve-path', function() {
-      return 'fake-path';
+      return 'resolved-path';
+    });
+
+    td.replace('../../../../../lib/targets/android/utils/sdk-root', function() {
+      return 'sdk-root'
     });
 
     let sdkPaths = require('../../../../../lib/targets/android/utils/sdk-paths');
     let paths = sdkPaths();
 
     expect(paths).to.deep.equal({
-      adb: path.join('fake-path', 'platform-tools', 'adb'),
-      emulator: path.join('fake-path')
+      adb: path.join('sdk-root', 'platform-tools', 'adb'),
+      emulator: path.join('resolved-path')
     });
+  });
+
+  it('errors in ANDROID_HOME is not set', function() {
+    td.replace('../../../../../lib/targets/android/utils/sdk-root', function() {
+      return undefined;
+    });
+
+    let logger = td.replace('../../../../../lib/utils/logger');
+
+    let sdkPaths = require('../../../../../lib/targets/android/utils/sdk-paths');
+    sdkPaths();
+
+    td.verify(logger.error(anything()));
   });
 });
