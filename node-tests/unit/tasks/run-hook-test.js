6bc2314a02438c15ad54638b9506e62f3b663953
Alex MacLeod <alexhmacleod@gmail.com>
2019-01-21 14:06:39 -0500

    refactor(run-hook-test): es6-ify

--- node-tests/unit/tasks/run-hook-test.js
+++ node-tests/unit/tasks/run-hook-test.js
@@ -1,40 +1,38 @@
-'use strict';
+const expect          = require('../../helpers/expect');
+const HookTask        = require('../../../lib/tasks/run-hook');
+const mockProject     = require('../../fixtures/corber-mock/project');
+const Promise         = require('rsvp').Promise;
 
-var expect          = require('../../helpers/expect');
-var HookTask        = require('../../../lib/tasks/run-hook');
-var mockProject     = require('../../fixtures/corber-mock/project');
-var Promise         = require('rsvp').Promise;
-
-describe('Run Hook Task', function() {
-  it('runs a hook at the provided path', function() {
-    var hookTask = new HookTask(mockProject);
+describe('Run Hook Task', () => {
+  it('runs a hook at the provided path', () => {
+    let hookTask = new HookTask(mockProject);
     return expect(hookTask.run('hook')).to.be.fulfilled;
   });
 
-  it('passes options to the hook', function() {
-    var options = {foo: true};
-    var hookTask = new HookTask(mockProject);
-    var taskRun = hookTask.run('hook-with-options', options);
+  it('passes options to the hook', () => {
+    let options = {foo: true};
+    let hookTask = new HookTask(mockProject);
+    let taskRun = hookTask.run('hook-with-options', options);
     return expect(taskRun).to.become(options);
   });
 
-  it('runs a hook at the provided path that has an error', function() {
-    var hookTask = new HookTask(mockProject);
+  it('runs a hook at the provided path that has an error', () => {
+    let hookTask = new HookTask(mockProject);
     return expect(hookTask.run('hook-with-error')).to.eventually.be.rejected;
   });
 
-  it('is resolved if the hook is resolved', function() {
-    var hookTask = new HookTask(mockProject);
-    var expectation = expect(hookTask.run('hook-promise-resolved'));
+  it('is resolved if the hook is resolved', () => {
+    let hookTask = new HookTask(mockProject);
+    let expectation = expect(hookTask.run('hook-promise-resolved'));
     return Promise.all([
       expectation.to.eventually.equal('resolved promise from hook'),
       expectation.to.eventually.be.fulfilled,
     ]);
   });
 
-  it('is rejected if the hook is rejected', function() {
-    var hookTask = new HookTask(mockProject);
-    var expectation = expect(hookTask.run('hook-promise-rejected'));
+  it('is rejected if the hook is rejected', () => {
+    let hookTask = new HookTask(mockProject);
+    let expectation = expect(hookTask.run('hook-promise-rejected'));
     return Promise.all([
       expectation.to.eventually.equal('hook rejected'),
       expectation.to.eventually.be.rejected,
