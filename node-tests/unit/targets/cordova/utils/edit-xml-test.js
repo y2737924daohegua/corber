3aa873e998c1be054c16e4919590864f4cbe33e0
Alex MacLeod <alexhmacleod@gmail.com>
2020-04-03 19:52:22 -0400

    fix(edit-xml): be careful with edit-config in android manifest

--- node-tests/unit/targets/cordova/utils/edit-xml-test.js
+++ node-tests/unit/targets/cordova/utils/edit-xml-test.js
@@ -1,72 +1,370 @@
 const cordovaPath     = require('../../../../../lib/targets/cordova/utils/get-path');
-const editXml         = require('../../../../../lib/targets/cordova/utils/edit-xml');
 const expect          = require('../../../../helpers/expect');
 const mockProject     = require('../../../../fixtures/corber-mock/project');
-const parseXml        = require('../../../../../lib/utils/parse-xml');
 const path            = require('path');
 const lodash          = require('lodash');
+const td              = require('testdouble');
+const xml2js          = require('xml2js');
+
+const parseHelper = (xml) => {
+  return new Promise(function(resolve, reject) {
+    let parser = new xml2js.Parser();
+
+    parser.parseString(xml, function(err, result) {
+      if (err) {
+        return reject(err);
+      }
+
+      if (result) {
+        resolve(result);
+      }
+    });
+  });
+};
 
 describe('Edit XML Util', function() {
-  const host = 'http://localhost:8080';
+  let editXml;
+  let parseXml;
+  let fs;
+
+  let cdvPath;
+  let configPath;
+  let project;
+
+  beforeEach(function() {
+    fs = td.replace('fs');
+    parseXml = td.replace('../../../../../lib/utils/parse-xml');
+
+    editXml = require('../../../../../lib/targets/cordova/utils/edit-xml');
+
+    project = mockProject.project;
+    cdvPath = cordovaPath(mockProject.project);
+    configPath = path.join(cdvPath, 'config.xml');
+  });
+
+  afterEach(function() {
+    td.reset();
+  });
 
   describe('addNavigation function', function() {
-    it('add node to the xml file in addition to client nodes', function() {
-      return editXml.addNavigation(mockProject.project, host).then(() => {
-        let cdvPath = cordovaPath(mockProject.project);
-        let configPath = path.join(cdvPath, 'config.xml');
-        let xml = parseXml(configPath);
-        let nodes = xml._result.widget['allow-navigation'].length;
+    it('adds allow-navigation node to the xml file', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: { '$': {} }
+      });
 
-        expect(nodes).to.equal(3);
+      return editXml.addNavigation(project, 'host').then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
+
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        const node = lodash.find(json.widget['allow-navigation'], {
+          '$': { name: 'ec-nav' }
+        });
+
+        expect(node).to.not.be.undefined;
+        expect(node.$.href).to.equal('host');
+      });
+    });
+
+    it('leaves existing allow-navigation nodes in place', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          'allow-navigation': [
+            { '$': { href: 'foo', name: 'bar' } }
+          ]
+        }
+      });
+
+      return editXml.addNavigation(project, 'host').then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
+
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        const node = lodash.find(json.widget['allow-navigation'], {
+          '$': { name: 'bar' }
+        });
+
+        expect(node).to.not.be.undefined;
+        expect(node.$.href).to.equal('foo');
       });
     });
   });
 
   describe('removeNavigation function', function() {
-    beforeEach(function() {
-      editXml.addNavigation(mockProject.project, host);
-      editXml.removeNavigation(mockProject.project);
+    it('removes ec-nav allow-navigation node from the xml file', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          'allow-navigation': [
+            { '$': { href: 'host', name: 'ec-nav' } }
+          ],
+        }
+      });
+
+      return editXml.removeNavigation(project).then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
+
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        const node = lodash.find(json.widget['allow-navigation'], {
+          '$': { name: 'ec-nav' }
+        });
+
+        expect(node).to.be.undefined;
+      });
     });
 
-    describe('if nodes placed by util exist', function() {
-      it('removes util placed nodes and keep client nodes', function() {
-        let cdvPath = cordovaPath(mockProject.project);
-        let configPath = path.join(cdvPath, 'config.xml');
-        let xml = parseXml(configPath);
-        let nodes = xml._result.widget['allow-navigation'].length;
+    it('does not remove other allow-navigation nodes', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          'allow-navigation': [
+            { '$': { href: 'host', name: 'ec-nav' } },
+            { '$': { href: 'foo', name: 'bar' } }
+          ]
+        }
+      });
 
-        expect(nodes).to.equal(2);
+      return editXml.removeNavigation(project).then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
+
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        const node = lodash.find(json.widget['allow-navigation'], {
+          '$': { name: 'bar' }
+        });
+
+        expect(node).to.not.be.undefined;
+        expect(node.$.href).to.equal('foo');
       });
     });
   });
 
   describe('addAndroidCleartext function', function() {
-    it('adds and removes android cleartext/http bypass', function() {
-      let cdvPath = cordovaPath(mockProject.project);
-      let configPath = path.join(cdvPath, 'config.xml');
+    it('adds xmlns:android if missing', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          $: {},
+          platform: [
+            { '$': { name: 'android'} }
+          ]
+        },
+      });
 
-      return editXml.addAndroidCleartext(mockProject.project).then(() => {
-        return parseXml(configPath).then((json) => {
-          let platforms = json.widget.platform;
-          let androidPlatform = lodash.find(platforms, { '$': { name: 'android' } });
-          let xmlnsAndroid = json.widget['$']['xmlns:android'];
+      return editXml.addAndroidCleartext(project).then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
 
-          expect(xmlnsAndroid).to.not.equal(undefined);
-          expect(androidPlatform['edit-config']).to.not.equal(undefined);
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        expect(json.widget.$['xmlns:android']).to.equal('http://schemas.android.com/apk/res/android');
+      });
+    });
 
-          return editXml.removeAndroidCleartext(mockProject.project).then(() => {
-            return parseXml(configPath).then((json) => {
+    it('leave xmlns:android in place if present', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          $: {
+            'xmlns:android': 'foo'
+          },
+          platform: [
+            { '$': { name: 'android' } }
+          ]
+        },
+      });
 
-              platforms = json.widget.platform;
-              androidPlatform = lodash.find(platforms, { '$': { name: 'android' } })
-              xmlnsAndroid = json.widget['$']['xmlns:android'];
+      return editXml.addAndroidCleartext(project).then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
 
-              expect(xmlnsAndroid).to.equal(undefined);
-              expect(androidPlatform['edit-config']).to.equal(undefined);
-            });
-          });
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        expect(json.widget.$['xmlns:android']).to.equal('foo');
+      });
+    });
+
+    it('adds clearText application tag if missing', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          $: {},
+          platform: [
+            { '$': { name: 'android' } }
+          ]
+        },
+      });
+
+      return editXml.addAndroidCleartext(project).then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
+
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        let androidPlatform = lodash.find(json.widget.platform, {
+          '$': { name: 'android' }
         });
-      })
+
+        expect(androidPlatform).to.not.be.undefined;
+
+        let editConfig = lodash.find(androidPlatform['edit-config'], {
+          '$': { file: 'app/src/main/AndroidManifest.xml' }
+        });
+
+        expect(editConfig).to.not.be.undefined;
+        expect(editConfig.application).to.not.be.undefined;
+
+        let application = lodash.find(editConfig.application, (app) => {
+          return app['$'].hasOwnProperty('android:usesCleartextTraffic');
+        });
+
+        expect(application).to.not.be.undefined;
+        expect(application['$']['android:usesCleartextTraffic']).to.not.be.undefined;
+      });
+    });
+
+    it('overwrites existing cleartext application tag if present', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          $: {},
+          platform: [
+            {
+              '$': { name: 'android' },
+              'edit-config': [
+                {
+                  '$': { file: 'app/src/main/AndroidManifest.xml' },
+                  application: [
+                    { $: { 'android:usesCleartextTraffic': 'foo' } }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+      });
+
+      return editXml.addAndroidCleartext(project).then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
+
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        let androidPlatform = lodash.find(json.widget.platform, {
+          '$': { name: 'android' }
+        });
+
+        expect(androidPlatform).to.not.be.undefined;
+
+        let editConfig = lodash.find(androidPlatform['edit-config'], {
+          '$': { file: 'app/src/main/AndroidManifest.xml' }
+        });
+
+        expect(editConfig).to.not.be.undefined;
+        expect(editConfig.application).to.not.be.undefined;
+
+        let application = lodash.find(editConfig.application, (app) => {
+          return app['$'].hasOwnProperty('android:usesCleartextTraffic');
+        });
+
+        expect(application).to.not.be.undefined;
+        expect(application['$']['android:usesCleartextTraffic']).to.equal('true');
+      });
+    });
+  });
+
+  describe('removeAndroidCleartext function', function() {
+    it('removes edit-config when no other application tags', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          $: {},
+          platform: [
+            {
+              '$': { name: 'android' },
+              'edit-config': [
+                {
+                  '$': { file: 'app/src/main/AndroidManifest.xml' },
+                  application: [
+                    { $: { 'android:usesCleartextTraffic': 'true' } }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+      });
+
+      return editXml.removeAndroidCleartext(project).then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
+
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        let androidPlatform = lodash.find(json.widget.platform, {
+          '$': { name: 'android' }
+        });
+
+        expect(androidPlatform).to.not.be.undefined;
+
+        let editConfig = lodash.find(androidPlatform['edit-config'], {
+          '$': { file: 'app/src/main/AndroidManifest.xml' }
+        });
+
+        expect(editConfig).to.be.undefined;
+      });
+    });
+
+    it('does not remove other application tags', function() {
+      td.when(parseXml(configPath)).thenResolve({
+        widget: {
+          $: {},
+          platform: [
+            {
+              '$': { name: 'android' },
+              'edit-config': [
+                {
+                  '$': { file: 'app/src/main/AndroidManifest.xml' },
+                  application: [
+                    { $: { 'android:usesCleartextTraffic': 'true' } },
+                    { $: { 'foo': 'bar' } }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+      });
+
+      return editXml.removeAndroidCleartext(project).then(() => {
+        let captor = td.matchers.captor();
+        td.verify(fs.writeFileSync(configPath, captor.capture()));
+
+        return parseHelper(captor.values[0]);
+      }).then((json) => {
+        let androidPlatform = lodash.find(json.widget.platform, {
+          '$': { name: 'android' }
+        });
+
+        expect(androidPlatform).to.not.be.undefined;
+
+        let editConfig = lodash.find(androidPlatform['edit-config'], {
+          '$': { file: 'app/src/main/AndroidManifest.xml' }
+        });
+
+        expect(editConfig).to.not.be.undefined;
+        expect(editConfig.application).to.not.be.undefined;
+
+        let application = lodash.find(editConfig.application, (app) => {
+          return app['$'].hasOwnProperty('android:usesCleartextTraffic');
+        });
+
+        expect(application).to.be.undefined;
+
+        application = lodash.find(editConfig.application, (app) => {
+          return app.$.hasOwnProperty('foo');
+        });
+
+        expect(application.$.foo).to.equal('bar');
+      });
     });
   });
 });
